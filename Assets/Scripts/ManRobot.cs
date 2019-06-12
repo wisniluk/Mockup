994c848464b36ef7f44b1e63a079892094ff0639
Łukasz Wiśniewski <lukasz9wisniewski5@gmail.com>
2019-06-12 20:59:39 +0200

    tak

---
+++ Assets/Scripts/ManRobot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ManRobot : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    Camera robotCamera;
+    public void setAgetnDestination(Vector3 des)
+    {
+        agent.destination = des;
+    }
+
+    public void stopAgent()
+    {
+        agent.isStopped = true;
+    }
+
+    public void runAgent()
+    {
+        agent.isStopped = false;
+    }
+
+    public bool objectInCamera(GameObject gameObject)
+    {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        return IsVisibleFrom(renderer, robotCamera);
+    }
+
+    private bool IsVisibleFrom(Renderer renderer, Camera camera)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+        return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+    }
+
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        robotCamera = GetComponentInChildren<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/RobotControler.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotControler : MonoBehaviour {
+
+    // Use this for initialization
+
+    public ManRobot robot;
+    public ManRobot cleaningRobot;
+    public Transform[] targets;
+    public GameObject przeszkoda;
+    public Transform respawn;
+    private int currentIndex = 0;
+    private bool stop = false;
+    private void setNextTarget()
+    {   
+        
+        if(!stop)
+        {
+            if (currentIndex <= targets.Length - 1)
+            {
+                float dist = Vector3.Distance(targets[currentIndex].position, robot.transform.position);
+                if (dist <= 4)
+                {
+                    currentIndex++;
+                    robot.setAgetnDestination(targets[currentIndex].position);
+                }
+            }
+            else
+            {
+                currentIndex = 0;
+                robot.setAgetnDestination(targets[currentIndex].position);
+            }
+        }
+        
+    }
+    void Start()
+    {
+
+        robot.setAgetnDestination(targets[currentIndex].position);
+    }
+
+    void sendCleanRobot()
+    {
+        float dist = Vector3.Distance(robot.transform.position, przeszkoda.transform.position);
+
+        if(dist <= 20 && robot.objectInCamera(przeszkoda))
+        {
+            stop = true;
+            robot.stopAgent();
+            cleaningRobot.setAgetnDestination(przeszkoda.transform.position);
+        }
+    }
+
+    void recalCleanRobot()
+    {
+        float dist = Vector3.Distance(cleaningRobot.transform.position, przeszkoda.transform.position);
+        if(dist <= 5)
+        {
+            przeszkoda.transform.position = new Vector3(przeszkoda.transform.position.x, -100f, przeszkoda.transform.position.z);
+            cleaningRobot.setAgetnDestination(respawn.position);
+            stop = false;
+            robot.runAgent();
+        }
+    }
+
+	// Update is called once per frame
+
+	void Update () {
+        setNextTarget();
+        if (przeszkoda != null)
+        {
+            sendCleanRobot();
+            recalCleanRobot();
+        }
+    }
+}
